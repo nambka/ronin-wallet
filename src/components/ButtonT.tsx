8bb24de195338220a6983777eb4a797333bb1baf
Nam Nguyen <namnguyen.sci@gmail.com>
2021-07-17 03:06:22 +0700

    feat: add button with icon and text

---
+++ src/components/ButtonT.tsx
@@ -0,0 +1,59 @@
+import React, { ReactNode } from 'react'
+import styled from 'styled-components'
+import { Heading } from "@chakra-ui/react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+
+type Props = {
+  label: string;
+  faIcon: IconProp;
+  onClick?: () => void;
+  children?: ReactNode;
+  disabled?: boolean;
+};
+
+const ButtonT = ({label, faIcon, onClick, disabled, children}: Props) => (
+  <StyledButton onClick={onClick} >
+      {children}
+      <Icon icon={faIcon} />
+      {/* <Heading as="h6" size="xs" style={{color:'#000', fontSize:'0.8rem'}}>{label}</Heading> */}
+      <Label>{label}</Label>
+  </StyledButton>
+)
+
+export default ButtonT
+
+const Icon = styled(FontAwesomeIcon)`
+  padding: 10px;
+  border-radius: 6px;
+  font-size: 2.75rem;
+`;
+
+const Label = styled.div`
+  margin-top: 5px;
+  color: #345;
+  font-size: 0.8rem;
+  font-weight: 600;
+`;
+
+const StyledButton = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+  /* padding: 10px; */
+  width: 60px;
+  height: 75px;
+  color: #2287ec;
+  cursor: pointer;
+	&:hover ${Icon} {
+    background-color: #edf1f7;
+  }
+	&:disabled ${Label} {
+    color: #8dc9f9;
+  }
+
+`;

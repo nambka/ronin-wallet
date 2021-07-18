aaafeb4f00c3ca9546cb6f35115eed89f9d3618c
Nam Nguyen <namnguyen.sci@gmail.com>
2021-07-18 11:19:57 +0700

    refactor: update style and layout

--- src/views/Unlock.tsx
+++ src/views/Unlock.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Text, InputGroup, Input, InputRightElement, Image } from "@chakra-ui/react"
+import { Text, InputGroup, Input, InputRightElement, Image, FormControl } from "@chakra-ui/react"
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 import Button from '../components/Button'
 import styled from 'styled-components'
@@ -7,14 +7,9 @@ import walletLogo from '../assets/images/logo.png'
 import { useHistory, Link } from 'react-router-dom'
 
 const Unlock: React.FC = () => {
-  const [inputPass, setinputPass] = useState('');
   const history = useHistory();
   const [show, setShow] = React.useState(false)
   const handleShow = () => setShow(!show)
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setinputPass(e.target.value);
-  }
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -24,15 +19,18 @@ const Unlock: React.FC = () => {
   return (
     <Box onSubmit={handleSubmit} >
       <Image id="wallet-logo" src={walletLogo} width={114} alt="wallet-logo"/>
-      <Text fontSize="3xl" fontWeight="700">Ronin Wallet</Text>
-      <Text fontSize="sm">Your Digital Passport</Text>
-      <InputGroup>
-        <Input type={show ? "text" : "password"} autoFocus
-              onChange={handleChange} placeholder="Type your password"/>
-        <InputRightElement width="4.5rem" onClick={handleShow}>
-            {show ? <ViewIcon color="#89a"/> : <ViewOffIcon color="#89a"/>}
-        </InputRightElement>
-      </InputGroup>
+      <Brand fontSize="3xl" fontWeight="700">Ronin Wallet</Brand>
+      <Slogan fontSize="sm">Your Digital Passport</Slogan>
+      <FControl id="password">
+        <LabelLeft>Enter Password</LabelLeft>
+        <InputGroup>
+          <Input type={show ? "text" : "password"} autoFocus
+                placeholder="Type your password"/>
+          <InputRightElement width="4.5rem" onClick={handleShow}>
+              {show ? <ViewIcon color="#89a"/> : <ViewOffIcon color="#89a"/>}
+          </InputRightElement>
+        </InputGroup>
+      </FControl>
       <Button type='submit'>Unlock</Button>
     </Box>
   )
@@ -40,14 +38,44 @@ const Unlock: React.FC = () => {
 
 export default Unlock
 
+const MainContainer = styled.div`
+  position: relative;
+  display: flex;
+`;
+
 const Box = styled.form`
   position: relative;
   display: flex;
   margin-left: auto;
   margin-right: auto;
+  width: 100%;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   margin-top: 4rem;
   padding: 20px;
+`;
+
+const Brand = styled(Text)`
+  margin-top: 24px;
+`;
+
+const Slogan = styled(Text)`
+  margin-top: 6px;
+`;
+
+const FControl = styled(FormControl)`
+  margin: 24px 0;
+`;
+
+const LabelInfo = styled(MainContainer)`
+  justify-content: space-between;
+  font-size: 0.65rem!important;
+  font-weight: 700;
+  color: #456;
+  text-transform: uppercase;
+`;
+
+const LabelLeft = styled(LabelInfo)`
+  margin: 0 0 5px 10px!important;
 `;
